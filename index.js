a69e0aaaaf87cf69ea1d7ee53eb6c8665421cc67
Guilherme <Fireguiquenbr@gmail.com>
2022-11-22 15:27:57 -0300

    update

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const puppeteer = require('puppeteer');
 
 // Inf 
 var url = "https://www.olx.com.br/"; 
-
+var path_browser = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
 
 (async() =>{ 
     const navegador = await puppeteer.launch({
+        executablePath: path_browser, 
         headless: false,
         userDataDir: 'credenciais'  
     });
     const pagina = await navegador.newPage(); 
     await pagina.goto(url);
-
+    
 
 }) ();
