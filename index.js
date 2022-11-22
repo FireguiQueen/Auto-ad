1cfbe74234c099de44db70b68478115ee1541f6f
Guilherme <Fireguiquenbr@gmail.com>
2022-11-22 14:31:40 -0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer');
+const pptr = require('puppeteer-core');
 
 (async() =>{ 
-    const navegador = await puppeteer.launch({
+    const navegador = await pptr.launch({
+        executablePath: "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
         headless: false
     });
     const pagina = await navegador.newPage(); 
